07b195897cb71b992109167a4427151586c93e30
Julius Fan <jzfan@ucsc.edu>
2017-01-21 01:39:47 -0800

    Hi There

--- src/com/soylentispeople/servernet/ConnectionReceiver.java
+++ src/com/soylentispeople/servernet/ConnectionReceiver.java
@@ -6,15 +6,21 @@ import java.io.IOException;
  * Created by thatq on 1/21/2017.
  */
 public class ConnectionReceiver implements Runnable{
-    public ConnectionReceiver(Socket [] s, ServerSocket serverSocket) throws IOException{
-        for (int i = 0; i < s.length; i++){
-            s[i] = serverSocket.accept();
-            //Hi There
-        }
+    private final Socket [] sockets;
+    private final ServerSocket serverSocket;
+    public ConnectionReceiver(Socket [] s, ServerSocket serverSocket) {
+        sockets = s;
+        this.serverSocket = serverSocket;
     }
 
     @Override
-    public void run(){
-
+    public void run() {
+        for (int i = 0; i < sockets.length; i++){
+            try {
+                sockets[i] = serverSocket.accept();
+            } catch (IOException e){
+                System.out.println("Connection Failed");
+            }
+        }
     }
 }
---
+++ src/com/soylentispeople/servernet/ConnectionReceiverListener.java
@@ -0,0 +1,7 @@
+package com.soylentispeople.servernet;
+
+/**
+ * Created by thatq on 1/21/2017.
+ */
+public interface ConnectionReceiverListener {
+}
