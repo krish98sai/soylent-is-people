edf64848752ea2f589e5d555d4a4f3780732e115
Saikrishna Rao <krish98.sai@gmail.com>
2017-01-21 02:16:04 -0800

    Update 2

--- src/com/soylentispeople/map/Polygon.java
+++ src/com/soylentispeople/map/Polygon.java
@@ -13,17 +13,7 @@ public class Polygon {
     public boolean intersects(Point p1, Point p2) {
         Line interLine = new Line(p1, p2);
         if(interLine.getSlope() == 0) {
-            for (int i = 0; i < points.length; i++) {
-                Line polyLine = new Line(points[i], points[(i + 1) % points.length]);
-
-                if(polyLine.getSlope() == 0) {
-                    if(points[i].y == p1.y) {
-                          if(points[i].x == p1.x && points[]) {
-
-                          }
-                    }
-                }
-            }
+            return interLineZero(p1, p2);
         } else if(Float.isInfinite(interLine.getSlope())) {
 
         } else {
@@ -31,6 +21,30 @@ public class Polygon {
         }
 
         return false;
+    }
+
+    private boolean interLineZero(Point p1, Point p2){
+        Point interMaxXPoint = getMaxXPoint(p1, p2);
+        Point interMinXPoint = interMaxXPoint == p1 ? p2 : p1;
+        for (int i = 0; i < points.length; i++) {
+            Line polyLine = new Line(points[i], points[(i + 1) % points.length]);
+
+            if(polyLine.getSlope() == 0) {
+                if(points[i].y == p1.y) {
+                    Point polyMaxXPoint = getMaxXPoint(points[i], points[(i + 1) % points.length]);
+                    Point polyMinXPoint = polyMaxXPoint == points[i] ? points[(i + 1) % points.length] : points[i];
+
+                    if((polyMaxXPoint.x > interMinXPoint.x) && (polyMinXPoint.x < interMaxXPoint.x)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private Point getMaxXPoint(Point p1, Point p2){
+        return p1.x > p2.x ? p1 : p2;
     }
 
     public Point[] getPoints() {
