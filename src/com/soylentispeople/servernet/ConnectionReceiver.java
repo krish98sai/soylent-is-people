0cf530303fc32c85dbd3c38d2761a963c4fe6531
Julius Fan <jzfan@ucsc.edu>
2017-01-21 01:14:53 -0800

    Hi There

---
+++ src/com/soylentispeople/servernet/ConnectionReceiver.java
@@ -0,0 +1,13 @@
+package com.soylentispeople.servernet;
+import java.net.ServerSocket;
+
+/**
+ * Created by thatq on 1/21/2017.
+ */
+public class ConnectionReceiver implements Runnable{
+    public ConnectionReceiver(Socket [] s, ServerSocket serverSocket){
+        for (int i = 0; i < s.length; i++){
+            s[i] = serverSocket.accept();
+        }
+    }
+}
